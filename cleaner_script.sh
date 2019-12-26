220abc1a24e8ceac81e6dc80020e656298de1fe8
Portergos Linux <portergoslinux@gmail.com>
2019-12-26 09:33:05 -0300

    Update cleaner_script.sh

--- cleaner_script.sh
+++ cleaner_script.sh
@@ -8,6 +8,8 @@
 # If multiples partitions are used
 chroot_path=$(lsblk |grep "calamares-root" |awk '{ print $7 }' |sed -e 's/\/tmp\///' -e 's/\/.*$//' |tail -n1)
 
+NEW_USER=$(cat /tmp/$chroot_path/etc/passwd | grep "/home" |cut -d: -f1 |head -1)
+
 arch_chroot(){
 # Use chroot not arch-chroot because of the way calamares mounts partitions
     chroot /tmp/$chroot_path /bin/bash -c "${1}"
@@ -17,27 +19,29 @@ arch_chroot(){
 
 # Copy any file from live environment to new system
 
-#local 
-_files_to_copy=(
+_copy_files(){
 
-/etc/os-release
-/etc/lightdm/*
-/etc/sddm.conf.d/kde_settings.conf
-/etc/pacman.d/hooks/*
-/etc/lsb-release
-/etc/default/grub
+    local _files_to_copy=(
 
+    /etc/os-release
+    /etc/lightdm/*
+    /etc/sddm.conf.d/kde_settings.conf
+    /etc/pacman.d/hooks/*
+    /etc/lsb-release
+    /etc/default/grub
 
 )
 
-#local xx
+    local xx
 
-for xx in ${_files_to_copy[*]}; do rsync -vaRI $xx /tmp/$chroot_path; done
+# Uses the entire file path and copies directly to / mounted point
+    for xx in ${_files_to_copy[*]}; do rsync -vaRI $xx /tmp/$chroot_path; done
+    cp -rf /etc/skel/.bashrc /tmp/$chroot_path/home/$NEW_USER/.bashrc
+    chown -R $NEW_USER:users /tmp/$chroot_path/home/$NEW_USER/.bashrc
 
-#cp -f /etc/os-release $chroot_path/etc/os-release
-#cp -rf /etc/lightdm $chroot_path/etc
-#cp -rf /etc/sddm.conf $chroot_path/etc
-#rsync -vaRI /etc/os-release /etc/lightdm/* /etc/sddm.conf.d/kde_settings.conf $chroot_path
+}
+
+_copy_files
 
 # For chrooted commands edit the script bellow directly
 arch_chroot "/usr/bin/chrooted_cleaner_script.sh"
