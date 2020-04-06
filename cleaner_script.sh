47cd7eeb03726bfa969871cce2e227b074245201
Portergos Linux <portergoslinux@gmail.com>
2020-04-06 17:58:38 -0300

    Update cleaner_script.sh

--- cleaner_script.sh
+++ cleaner_script.sh
@@ -31,9 +31,6 @@ arch_chroot(){
 cp -rf /etc/skel/.bashrc /tmp/$chroot_path/home/$NEW_USER/.bashrc
 chown -R $NEW_USER:users /tmp/$chroot_path/home/$NEW_USER/.bashrc
 
-}
-
-
 _copy_files(){
 
     local _files_to_copy=(
