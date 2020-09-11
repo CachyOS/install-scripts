3e507c7accc5b5480b81c5db00d0b23368ff0f7e
Johannes Kamprad <archlinux@kamprad.net>
2020-09-11 11:19:40 +0200

    Update chrooted_cleaner_script.sh
    fix typo

--- chrooted_cleaner_script.sh
+++ chrooted_cleaner_script.sh
@@ -356,24 +356,16 @@ _de_wm_config(){
         esac
     done
 }
-
-
-}
-
 _remove_gnome-software(){
     local gnome-software="$1"
     pacman -Q $gnome-software >& /dev/null && {
         pacman -Rsn $gnome-software --noconfirm >/dev/null
-    }
-    
- 
 }
-
 _remove_discovery(){
     local discovery="$1"
     pacman -Q $discovery >& /dev/null && {
         pacman -Rsn $discovery --noconfirm >/dev/null
-    }
+}
 ########################################
 ########## SCRIPT STARTS HERE ##########
 ########################################
