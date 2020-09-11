907f161caa5c15aa7f13ca755613125d5e53332d
manuel-192 <manuel-192@outlook.com>
2020-09-11 20:06:33 +0300

    Update chrooted_cleaner_script.sh

--- chrooted_cleaner_script.sh
+++ chrooted_cleaner_script.sh
@@ -377,7 +377,6 @@ _vbox
 _vmware
 _de_wm_config
 _clean_up
-_remove_gnome_software
-_remove_discovery
+_remove_some_packages
 
 rm -rf /usr/bin/{calamares_switcher,cleaner_script.sh,chrooted_cleaner_script.sh,calamares_for_testers,rank_pacman_key.sh,pacstrap_calamares,update-mirrorlist}
